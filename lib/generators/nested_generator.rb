ec7b6cd36519f92c21def77f142f0128f61097fb
Roberto Sanchez Sanchez <roberto.sanchez@crowdint.com>
2010-07-27 17:14:35 -0500

    adding the helper template for nested fields

--- lib/generators/nested_generator.rb
+++ lib/generators/nested_generator.rb
@@ -4,6 +4,7 @@ class NestedGenerator < Rails::Generators::Base
   def install
     # Copy the unobtrusive JS file
     copy_file('nested-fields.js', 'public/javascripts/nested-fields.js')
+    copy_file('nested_fields_helper.rb', 'app/helpers/nested_fields_helper.rb')
   end
 
   def self.source_root
