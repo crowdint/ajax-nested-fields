266b4c00da7a618349857f32f8eed8d136d93ce7
David Padilla <david@crowdint.com>
2011-04-26 16:15:12 -0500

    Removed unused template files from generators

--- lib/generators/ajax_nested_generator.rb
+++ lib/generators/ajax_nested_generator.rb
@@ -1,10 +1,9 @@
 require 'rails/generators'
 
-class NestedGenerator < Rails::Generators::Base
+class AjaxNestedGenerator < Rails::Generators::Base
   def install
     # Copy the unobtrusive JS file
     copy_file('nested-fields.js', 'public/javascripts/nested-fields.js')
-    copy_file('nested_fields_helper.rb', 'app/helpers/nested_fields_helper.rb')
   end
 
   def self.source_root
