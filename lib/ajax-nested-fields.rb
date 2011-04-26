23c66652971b9ac0d98c15af4bee59f9a70ff9a5
David Padilla <david@crowdint.com>
2011-04-26 12:49:54 -0500

    Removed lines of unused code

--- lib/ajax-nested-fields.rb
+++ lib/ajax-nested-fields.rb
@@ -1,10 +1,4 @@
 require 'action_view'
 require 'ajax-nested-fields/helpers'
 
-# module NestedFields
-  # def self.included(base)
-    # base.extend(ClassMethods)
-  # end
-# end
-
 ActionView::Base.send :include, AjaxNestedFields::Helpers
